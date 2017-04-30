0dd2d0c082d1de93e66a158f7bad2413d99d4ccc
Gayan.Kanishka <gaya0007@gmail.com>
2017-04-30 21:29:46 +1000

    Initial

---
+++ Forex/TradingStratergies/TS0/TS0/src/main.py
@@ -0,0 +1,8 @@
+from src.DataDownloader import TickDownloader
+import datetime
+
+dwnldr = TickDownloader()
+
+start_date = datetime.date(2017,4,10)
+end_date = datetime.date(2017,4,30)
+dwnldr.download_historical_data('EUR_USD', start_date, end_date)
